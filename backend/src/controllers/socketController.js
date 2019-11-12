4b24ff0cd9d4be5ba4ca1677619094d9f4be8e93
unknown <jasonyeh@cpp.edu>
2019-11-11 19:18:05 -0800

    Added a socket manager, and added test socket events into Room

--- backend/src/controllers/socketController.js
+++ backend/src/controllers/socketController.js
@@ -1,23 +1,35 @@
-var express = require("express");
-var router = express.Router();
-var bodyParser = require('body-parser')
-var io = require('socket.io')(server);
+module.exports = {
+    startCommunication : function(io) {
+        console.log("Start socket connection");
+        const comms = io.of("/communication");
 
-server.listen(80);
+        comms.on('connection', function(socket) {
+            console.log('User has connected');
+            socket.join(socket.pin);
+            
+            socket.on('host-join-up', function(data) {
+                //Emit down event to room
+                console.log("Room has been created, and host joined");
+                socket.join(data.pin);
+            });
+    
+            socket.on('user-join-up', function(data) {
+                //Emit down event to room  
+                console.log("user-join-up");          
+            });
+    
+            socket.on('add-song-up', function(data) {
+                //Emit down event to room
+                console.log("add-song-up");
+            });
+    
+            socket.on('update-queue-up', function(data) {
+                //Emit down event to room
+                console.log("update-queue-up");
+            });
+        })
 
-io.on('connection', function(socket) {
-
-    socket.on('host-join', (data) => {
-        socket.join(data.pin);
-    })
-
-    socket.on('user-join', (data) => {
-        socket.join(data.pin);
-    })
-
-    socket.on('add-song', (data) => {
-        //add song to database
-    })
-})
-
-module.exports = router;
+        
+        
+    }
+}
