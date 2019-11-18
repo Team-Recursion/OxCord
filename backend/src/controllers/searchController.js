5042331adf2bf34f4ec7d9b8ec8dcdbdc474d6d8
Jesse Alcocer <alcocerjesse98@gmail.com>
2019-11-18 13:41:10 -0800

    changed API key

--- backend/src/controllers/searchController.js
+++ backend/src/controllers/searchController.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser')
 var {google} = require('googleapis');
 var youtube = google.youtube({
     version: 'v3',
-   auth: "AIzaSyB9kVxbAGJf9R3n2mWPWk6wzrXvEc2lmyI"
+   auth: "AIzaSyAnv9wdz7V6Ae7612mwZ9H2sUgOqiykH8A"
 });
 
 const yehAuth = "AIzaSyC2flXo37atInysZLuWt1dFIYGut4MNzeE"
